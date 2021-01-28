1ea727ca3e75cee0ba8a196f19e6e18da68ac616
xuanhuaz <kaowofeixiang@gmail.com>
2021-01-28 17:20:14 -0500

    updated Label

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-let express = require("express");
-let app = express();
-let port = 8002;
+function label(label){
+    Labelbox.setLabelForAsset(label).then(() => {
+        Labelbox.fetchNextAssetToLabel();
+    });
+}
 
-app.use(express.static("./"));
-
-app.listen(port, function(){
-    console.log(`Listening at http://localhost:${port}`);
-});
+Labelbox.currentAsset().subscribe((asset) => {
+    if (asset){
+        drawItem(asset.data);
+    }
+})
+function drawItem(dataToLabel){
+    const labelForm = `
+    <img src="${dataToLabel}" style="width: 300px;"></img>
+    <div style="display: flex;">
+      <button onclick="label('Red_Apple')">Red Apple</button>
+      <button onclick="label('Green_Apple')">Green Apple</button>
+    </div>
+  `;
+    document.querySelector('#form').innerHTML = labelForm;
+}
